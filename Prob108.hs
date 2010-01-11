5b409d7d46e385eb47260731701e136246c47a0c
Adrian Istrate <adr_istrate@yahoo.com>
2010-01-11 18:36:15 +0100

    Problem 108

---
+++ Prob108.hs
@@ -0,0 +1,19 @@
+-- 1/x + 1/y = 1/n
+-- n(x + y) = xy
+-- y(x - n) = nx
+-- y = nx / (x - n)
+
+import TestHelper
+
+
+solutions :: Integer -> [(Integer, Integer)]
+solutions n = [ (x, y) | x <- [n+1..2*n], (n * x) `mod` (x - n) == 0, 
+                                          let y = (n * x) `div` (x - n) ]
+
+result = head . dropWhile ((<= 1000) . snd) 
+         $ map (\n -> (n, length (solutions n))) [12,24..]
+
+main = printTime $ print result
+-- (180180,1013)
+-- Time: 532.384661 sec.
+-- (8min 52s, compiled)
---
+++ Prob108a.hs
@@ -0,0 +1,28 @@
+-- 1/x + 1/y = 1/n
+-- n(x + y) = xy
+-- y(x - n) = nx
+-- y = nx / (x - n)
+
+import TestHelper
+
+
+solutions :: Integer -> [(Integer, Integer)]
+solutions n = [ (x, y) | x <- [n+1..2*n], (n * x) `mod` (x - n) == 0, 
+                                          let y = (n * x) `div` (x - n) ]
+
+
+primes = [1, 2, 3, 5, 7, 11]
+  -- [2, 3, 5, 7, 11, 13, 17, 19, 23]
+
+solutions2 ps = (product (map (\n -> 2*n + 1) ps) + 1) `div` 2
+
+--allMultiples 1 ps = map (:[]) ps
+--allMultiples n ps = 
+
+result = head . dropWhile ((<= 1000) . snd) 
+         $ map (\n -> (n, length (solutions n))) [12,24..]
+
+main = printTime $ print result
+-- (180180,1013)
+-- Time: 532.384661 sec.
+-- (8min 52s, compiled)
