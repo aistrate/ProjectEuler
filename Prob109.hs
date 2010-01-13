ac8dc7a2016ece78b6020b9af51386a6f8bed924
Adrian Istrate <adr_istrate@yahoo.com>
2010-01-13 15:30:28 +0100

    Problem 109 (my 100th problem solved; reached Level 3)

---
+++ Prob109.hs
@@ -0,0 +1,25 @@
+import TestHelper
+
+
+data Dart = Miss | S Int | D Int | T Int
+          deriving (Eq, Show)
+
+score Miss = 0
+score (S r) = r
+score (D r) = 2 * r
+score (T r) = 3 * r
+
+regions = [1..20] ++ [25]
+
+doubles = map D regions
+allDarts = Miss : map S regions ++ doubles ++ map T (init regions)
+
+sameOrAfter d = dropWhile (/= d) allDarts
+
+checkouts = [ (a, b, c) | a <- allDarts, b <- sameOrAfter a, c <- doubles ]
+
+scoreCheckout (a, b, c) = score a + score b + score c
+
+
+result = length . filter (< 100) $ map scoreCheckout checkouts
+-- 38182
