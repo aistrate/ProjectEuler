a11b6af4b1d662fbad43011706a875db74bcb00f
Adrian Istrate <adr_istrate@yahoo.com>
2010-01-13 19:08:13 +0100

    Problem 111 (unsolved)

---
+++ Prob111.hs
@@ -0,0 +1,20 @@
+import Prob003 (primeNumbers)
+import TestHelper
+
+
+calcS :: [Integer] -> Int -> Integer
+calcS ns d = let c = head $ show d
+                 rs = map (\n -> (n, length . filter (== c) $ show n)) ns
+                 m = maximum $ map snd rs
+             in sum . map fst $ filter ((m ==) . snd) rs
+
+primesInterval :: Integer -> Integer -> [Integer]
+primesInterval m n = filter (\k -> not $ any (`divOf` k)
+                                   (takeWhile (<= (intSqrt n)) primeNumbers)) [m..n]
+  where a `divOf` b = b `mod` a == 0
+        intSqrt n   = round . sqrt $ fromIntegral n
+
+
+result = sum $ map (calcS (primesInterval 100000 1000000)) [0..9]
+
+main = printTime $ print result
