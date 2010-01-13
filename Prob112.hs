7d9255ec4dc244c123ecd6aa66bb73c922b10778
Adrian Istrate <adr_istrate@yahoo.com>
2010-01-13 16:28:23 +0100

    Problem 112

---
+++ Prob112.hs
@@ -0,0 +1,18 @@
+import Data.List (sort)
+import TestHelper
+
+isBouncy :: Integer -> Bool
+isBouncy n = let r = show n
+                 s = sort r
+             in r /= s && r /= reverse s
+
+bouncyNumbers = filter isBouncy [1..]
+
+proportions = zipWith (\k n -> (k, n, fromIntegral k / fromIntegral n))
+                      [1..] bouncyNumbers
+
+result = head $ filter (\(_, _, p) -> p == 0.99) proportions
+
+main = printTime $ print result
+-- (1571130,1587000,0.99)
+-- Time: 13.094756 sec.
