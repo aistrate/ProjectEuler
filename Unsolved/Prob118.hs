24187e447faa8ca3673ba962f87a6387de9b0243
Adrian Istrate <adr_istrate@yahoo.com>
2010-01-15 15:51:02 +0100

    Move unsolved problems to a separate folder

---
+++ Unsolved/Prob118.hs
@@ -0,0 +1,11 @@
+import Data.Char (ord)
+import Data.List (nub)
+import TestHelper
+import Prob003
+
+
+digits :: Integer -> [Int]
+digits n = map (\c -> fromIntegral(ord c - ord '0')) $ show n
+
+candidatePrimes = filter (\ds -> ds == nub ds). map digits . takeWhile (< 987654321) 
+                  $ primeNumbers
