ff1e296256caa9bee7fb0428dd23d796609b7eba
Adrian Istrate <adr_istrate@yahoo.com>
2010-01-15 15:40:54 +0100

    Problem 121

---
+++ Prob121.hs
@@ -0,0 +1,24 @@
+import Data.Ratio
+import Data.List ((\\))
+import TestHelper
+import Prob012 (combinations)
+
+
+probabilities = map (1 %) [2..]
+
+probability n k = let ps = take n probabilities
+                      cs = combinations k ps
+                      anti c = map ((1 % 1) -) (ps \\ c)
+                      probForCase c = product (c ++ anti c)
+              in sum $ map probForCase cs
+
+totalProbability n = sum $ map (probability n) [((n `div` 2) + 1)..n]
+
+maxPrize n = let p = totalProbability n
+             in (denominator p) `div` (numerator p)
+
+
+main = printTime . print $ maxPrize 15
+-- 2269
+-- Time: 16.953125 sec.
+-- (compiled)
